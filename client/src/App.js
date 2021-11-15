bc3dbb6ed6625784225d9417a2de97d59abb7252
Diana Salas <salasd10418@gmail.com>
2021-11-14 20:24:36 -0500

    added necessary imports / added page structure / had to use Routes instead of Switch / need to figure out why links are not rendering pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,54 @@
-import './App.css';
-import redirectToLogin from './utils/AuthenticationKroger';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Nav from './components/Nav';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is going to be an app for volunteering to deliver groceries to local community members who are elderly, disabled, or veterans who have a difficult time leaving home or paying for grocery delivery.
-        </p>
-        <button onClick={redirectToLogin}>Sign In</button>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+            <Nav />
+            {/* <p>
+              This is going to be an app for volunteering to deliver groceries to local community members who are elderly, disabled, or veterans who have a difficult time leaving home or paying for grocery delivery.
+            </p> */}
+            <Routes>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
