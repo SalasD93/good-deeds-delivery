bb36dd6264d913c1adf777419e77ad705676e64f
Diana Salas <salasd10418@gmail.com>
2021-11-16 12:16:35 -0500

    added cart component to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CategoryMenu from "../components/CategoryMenu";
 import ProductList from "../components/ProductList";
+import Cart from "../components/Cart";
 
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <ProductList />
+      <Cart />
     </div>
   );
 };
