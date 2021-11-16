6435e35f6afd5f0371b0873ee0009d9d5fca27b8
Diana Salas <salasd10418@gmail.com>
2021-11-16 12:45:36 -0500

    added order history for users

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import CartDetail from './pages/CartDetail';
 import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -48,6 +49,7 @@ function App() {
               <Route exact path="/signup" element={<Signup />} />
               <Route exact path="/products:id" element={<CartDetail />} />
               <Route exact path="/success" element={<Success />} />
+              <Route exact path="/OrderHistory" element={<OrderHistory />} />
             </Routes>
           </StoreProvider>
         </div>
