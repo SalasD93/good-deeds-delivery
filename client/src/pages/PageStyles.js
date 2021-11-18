38d746457f1f0c7fa5ff1910f17a85fb54727ec2
Diana Salas <salasd10418@gmail.com>
2021-11-17 22:21:41 -0500

    added file for page styles / styled form for signup page / removed strict mode to remove error

---
+++ client/src/pages/PageStyles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    margin-top: 5%;
+    color: #104a36;
+`;
+
+export const FormH2 = styled.h2`
+    align-self: center;
+    text-decoration: underline;
+`;
+
+export const FormCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    background-color: #dce8e6;
+    padding: 20px 30px;
+    border-radius: 20px;
+`;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import { Button, Form } from 'semantic-ui-react';
+import { FormContainer, FormH2, FormCard } from './PageStyles';
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -31,55 +33,55 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <div>
+      <Link to="/login"><Button>← Go to Login</Button></Link>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <FormContainer>
+        <FormCard>
+          <FormH2>SIGNUP</FormH2>
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Field>
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                placeholder="First"
+                name="firstName"
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                placeholder="Last"
+                name="lastName"
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <div>
+              <Button type="submit">SIGNUP</Button>
+            </div>
+          </Form>
+        </FormCard>
+      </FormContainer>
     </div>
   );
 }
