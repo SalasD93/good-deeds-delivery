256cd05dd3136dc8c7671cbc9de250ff5a518115
Diana Salas <salasd10418@gmail.com>
2021-11-18 03:22:21 -0500

    added flex box/styles to products list

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { ProductsContainer } from './styles';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -44,24 +45,24 @@ function ProductList() {
   }
 
   return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
-      {state.products.length ? (
-        <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
-        </div>
-      ) : (
-        <h3>You haven't added any products yet!</h3>
-      )}
+    <div>
+      <h2>OUR PRODUCTS</h2>
+        {state.products.length ? (
+          <ProductsContainer>
+            {filterProducts().map((product) => (
+              <ProductItem
+                key={product._id}
+                _id={product._id}
+                image={product.image}
+                name={product.name}
+                price={product.price}
+                quantity={product.quantity}
+              />
+            ))}
+          </ProductsContainer>
+        ) : (
+          <h3>You haven't added any products yet!</h3>
+        )}
       {loading ? <img src={spinner} alt="loading" /> : null}
     </div>
   );
